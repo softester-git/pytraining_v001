a6cbae23924879d27450faedccc195ab3ab0a1d9
sash <softester@yandex.ru>
2021-06-24 22:16:36 +0300

    Удалены лишние методы

--- test_case_001.py
+++ test_case_001.py
@@ -33,17 +33,6 @@ class CreateGroup(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
 
 
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-
     def tearDown(self):
         self.wd.quit()
 
