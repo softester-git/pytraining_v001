b754270818df8d6b93406faa5b2a0a46e546cdf9
sash <softester@yandex.ru>
2021-06-24 22:24:11 +0300

    Добавлены комментарии

--- test_case_001.py
+++ test_case_001.py
@@ -10,7 +10,9 @@ class CreateGroup(unittest.TestCase):
     
     def test_create_group(self):
         wd = self.wd
+        # open main page
         wd.get("http://localhost/addressbook/index.php")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -18,8 +20,11 @@ class CreateGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+        # init create group
         wd.find_element_by_name("new").click()
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("group_name_001")
@@ -27,9 +32,12 @@ class CreateGroup(unittest.TestCase):
         wd.find_element_by_name("group_header").send_keys("group_header_001")
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("group_footer_001")
+        # submit form
         wd.find_element_by_name("submit").click()
+        # return to groups page
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_xpath("//div[@id='content']/form/span").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
 
 
