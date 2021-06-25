27e413158871170e525c5ed2349c008899d536bc
alexander.trukhachev <softester@yandex.ru>
2021-06-25 19:26:34 +0300

    Созданы 3 пакета для фикстуры, моделей и тестов

---
+++ test/test_case_002.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.contact import Contact
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_create_contact(app):
+    app.login(user_name="admin",user_pass="secret")
+    app.fill_form(Contact(contact_title="title1", contact_company="company1", contact_address="address1"))
+    app.logout()
+
+
+def test_create_empty_contact(app):
+    app.login(user_name="admin",user_pass="secret")
+    app.fill_form(Contact(contact_title="", contact_company="", contact_address=""))
+    app.logout()
