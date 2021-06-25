e84a0156bd0e3652aba5b68f3cb9fbb541fa506c
sash <softester@yandex.ru>
2021-06-25 03:02:59 +0300

    Переведено на pytest

--- test_case_001.py
+++ test_case_001.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
 from application import Application
 
 
-class CreateGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    
-    def test_create_group(self):
-        self.app.login(user_name="admin", user_pass="secret")
-        self.app.fill_group_form(Group(group_name="group_name_001", group_header="group_header_001", group_footer="group_footer_001"))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def test_create_empty_group(self):
-        self.app.login(user_name="admin", user_pass="secret")
-        self.app.fill_group_form(Group(group_name="", group_header="", group_footer=""))
-        self.app.logout()
+def test_create_group(app):
+    app.login(user_name="admin", user_pass="secret")
+    app.fill_group_form(Group(group_name="group_name_001", group_header="group_header_001", group_footer="group_footer_001"))
+    app.logout()
 
 
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_create_empty_group(app):
+    app.login(user_name="admin", user_pass="secret")
+    app.fill_group_form(Group(group_name="", group_header="", group_footer=""))
+    app.logout()
