a5860b337cb5df52883e4afd0c41538da3c5b16b
alexander.trukhachev <softester@yandex.ru>
2021-06-26 19:52:18 +0300

    Использование модели Contact

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -13,11 +13,13 @@ def app(request):
 
 def test_create_contact(app):
     app.login(user_name="admin",user_pass="secret")
+    # Использование модели Contact
     app.fill_form(Contact(contact_title="title1", contact_company="company1", contact_address="address1"))
     app.logout()
 
 
 def test_create_empty_contact(app):
     app.login(user_name="admin",user_pass="secret")
+    # Использование модели Contact
     app.fill_form(Contact(contact_title="", contact_company="", contact_address=""))
     app.logout()
