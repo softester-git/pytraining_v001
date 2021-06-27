cd392935134982d4fe338f5efa3329c70307e594
alexander.trukhachev <softester@yandex.ru>
2021-06-27 19:42:22 +0300

    Созданы вспомогательные классы SessionHelper и GroupHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
-import os
-from selenium.webdriver.support.select import Select
+from fixture.contact import ContactHelper
+
 
 class Application:
 
@@ -10,106 +10,13 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-        self.session = GroupHelper(self)
-
-    def submit_form(self):
-        wd = self.wd
-        #wd.find_element_by_name("submit").click()
-        wd.find_element_by_xpath("//input[@name='submit']").click()
-
-    def open_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def return_to_home(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_main_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
 
-    def fill_contact_form(self, contact):
-        wd = self.wd
-        self.open_contact_page()
-        # fill text fields
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.fname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("MiddleName")
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Nickname")
-        #wd.find_element_by_name("photo").click()
-        #wd.find_element_by_xpath("//input[@type='file']").click()
-        # wd.find_element_by_name("photo").clear()
-        wd.find_element_by_name("photo").send_keys(os.path.abspath('facebook.png'))
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Title")
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Company")
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.addr)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("home")
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("mobile_001")
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("work_001")
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys("fax_001")
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys("email2")
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys("email3")
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys("homepage")
-        # fill select fields
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1977")
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("5")
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("March")
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("1900")
-        wd.find_element_by_name("new_group").click()
-        Select(wd.find_element_by_name("new_group")).select_by_visible_text("group_name_1")
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("address_001")
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("home")
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("notes_001")
-        self.submit_form()
-        self.return_to_home()
 
     def destroy(self):
         wd = self.wd
--- test/test_contacts.py
+++ test/test_contacts.py
@@ -13,13 +13,11 @@ def app(request):
 
 def test_create_contact(app):
     app.session.login(user_name="admin", user_pass="secret")
-    # Использование модели Contact
-    app.fill_contact_form(Contact(fname="FirstName", lname="LastName", addr="Address", email="test@test.test"))
+    app.contact.create(Contact(fname="FirstName", lname="LastName", addr="Address", email="test@test.test"))
     app.session.logout()
 
 
 #def test_create_empty_contact(app):
-#    app.session.login(user_name="admin", user_pass="secret")
-    # Использование модели Contact
-#    app.fill_contact_form(Contact(fname="", lname="", addr="", email=""))
-#    app.session.logout()
+    app.session.login(user_name="admin", user_pass="secret")
+    app.contact.create(Contact(fname="", lname="", addr="", email=""))
+    app.session.logout()
