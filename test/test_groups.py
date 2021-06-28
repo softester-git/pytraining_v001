0b81cdbd4e521f8a0ecc1239641f03238ba6598e
alexander.trukhachev <softester@yandex.ru>
2021-06-28 15:25:01 +0300

    Исправлены ошибки

--- test/test_groups.py
+++ test/test_groups.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-
+import time
 
 def test_create_group(app):
     app.session.login(user_name="admin", user_pass="secret")
     app.group.create(Group(group_name="group_name_1", group_header="group_header_1", group_footer="group_footer_1"))
     app.session.logout()
+    time.sleep(3)
 
-
-#def test_create_empty_group(app):
-#    app.session.login(user_name="admin", user_pass="secret")
-#    app.group.create(Group(group_name="", group_header="", group_footer=""))
-#    app.session.logout()
+def test_create_empty_group(app):
+    app.session.login(user_name="admin", user_pass="secret")
+    app.group.create(Group(group_name="", group_header="", group_footer=""))
+    app.session.logout()
