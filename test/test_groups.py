e44496cfdfb881d85e5ab3bac344c320c416605b
alexander.trukhachev <softester@yandex.ru>
2021-07-01 16:18:32 +0300

    Создан метод получения списка групп и добавлена проверка

--- test/test_groups.py
+++ test/test_groups.py
@@ -9,4 +9,7 @@ def test_create_group(app):
     assert len(old_groups) + 1 == len(new_groups)
 
 def test_create_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(group_name="", group_header="", group_footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
