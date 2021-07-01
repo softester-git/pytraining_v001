e8557281a73fb036a313a45c8e6f04a69a9984c7
alexander.trukhachev <softester@yandex.ru>
2021-07-02 00:03:49 +0300

    Добавлено сравнение d

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
     def __init__(self, fname=None, mname=None, lname=None, nname=None, photo=None, title=None, company=None, addr=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bmonth=None, bday=None, amonth=None, aday=None, byear=None, ayear=None, address2=None, phone2=None, notes=None, id=None):
         self.fname = fname
@@ -32,3 +35,9 @@ class Contact:
 
     def __eq__(self, other):
         return(self.id is None or other.id is None or self.id == other.id and self.fname == other.fname and self.lname == other.lname)
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_contacts.py
+++ test/test_contacts.py
@@ -4,9 +4,12 @@ from model.contact import Contact
 
 def test_create_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.create(Contact(fname="FirstName", lname="LastName", addr="Address", email="test@test.test"))
+    contact = Contact(fname="FirstName", lname="LastName", addr="Address", email="test@test.test")
+    app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_create_empty_contact(app):
