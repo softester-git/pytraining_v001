53c385be8739b29a00532bd26563b4c15226bf4a
alexander.trukhachev <softester@yandex.ru>
2021-07-01 16:27:38 +0300

    Добавлена проверка количества групп перед и после добавления/редактирования/удаления группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,4 +5,7 @@ from model.group import Group
 def test_edit_group(app):
     if app.group.count() == 0:
         app.group.create(Group(group_name="Test"))
-    app.group.edit_first_group(Group(group_name="new_group"))
+    old_groups = app.group.get_group_list()
+    app.group.edit_first_group(Group(group_name="new_group", group_header="new_group_header", group_footer="new_group_footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
