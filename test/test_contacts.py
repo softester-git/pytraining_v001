05450e98485e0d5010aec9f1228f2781ca6ae29b
alexander.trukhachev <softester@yandex.ru>
2021-07-05 14:00:47 +0300

    Кол-во создаваемых параметризованных контактов уменьшено до 8. В наборе символов для генерации удалены пробелы и спецсимволы

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -6,19 +6,14 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 test_data = [
     Contact(fname=fname, lname=lname, addr=addr, email=email, phone2=phone2, home=home, work=work, mobile=mobile)
     for fname in ["", random_string("fname", 10)]
     for lname in ["", random_string("lname", 10)]
-    for addr in ["", random_string("addr", 10)]
     for email in ["", random_string("email", 10)]
-    for phone2 in ["", random_string("phone2", 10)]
-    for home in ["", random_string("home", 10)]
-    for work in ["", random_string("work", 10)]
-    for mobile in ["", random_string("mobile", 10)]
 ]
 
 @pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
