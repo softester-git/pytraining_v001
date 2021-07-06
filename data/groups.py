a10d4d5c61e5e50b51834c8cfbd8f80d0a252e60
alexander.trukhachev <softester@yandex.ru>
2021-07-06 17:10:59 +0300

    Добавлена динамическая параметризация

---
+++ data/groups.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+test_data = [
+    Group(group_name="name1", group_header="header1", group_footer="footer1"),
+    Group(group_name="name2", group_header="header2", group_footer="footer2")
+]
--- test/test_groups.py
+++ test/test_groups.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-from time import sleep
-import pytest
-from data.add_group import test_data
 
 
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
-def test_create_group(app, group):
+def test_create_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     old_groups_count = app.group.count()
     app.group.create(group)
-    #app.wd.find_element_by_name("new")
     assert old_groups_count + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
