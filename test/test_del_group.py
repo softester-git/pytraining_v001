a8b63ad033786aa3bc928eefd55bcb81f38f6f95
alexander.trukhachev <softester@yandex.ru>
2021-07-07 21:07:45 +0300

    Исправлена проверка объектов после удаления группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,19 +1,13 @@
 from model.group import Group
-from random import randrange
+import random
 
 def test_delete_some_group(app, db):
     if app.group.count() == 0:
         app.group.create(Group(group_name="Test"))
     old_groups = db.get_group_list()
     old_groups_count = app.group.count()
-    index = randrange(old_groups_count)
-    deleted_group_id = old_groups[index].group_id
+    index = random.choice(list(map(lambda x: x.group_id, old_groups)))
     app.group.delete_group_by_index(index)
     new_groups = db.get_group_list()
     assert old_groups_count - 1 == app.group.count()
-    eq_old_groups = []
-
-    for gr in old_groups:
-        if gr.group_id != deleted_group_id:
-            eq_old_groups.append(Group(group_id=gr.group_id, group_name=gr.group_name))
-    assert eq_old_groups == new_groups
+    assert list(filter(lambda s: s.group_id != index, old_groups)) == new_groups
