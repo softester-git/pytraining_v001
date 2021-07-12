cb859783f2bf678da125cafd25d258c280527220
sash <softester@yandex.ru>
2021-07-12 11:19:37 +0300

    Исправлена ошибка в проверке котактов

--- test/test_check_contact.py
+++ test/test_check_contact.py
@@ -3,8 +3,8 @@ def test_contact_home_page_eq_edit_page(app, db):
     contacts_home = app.contact.get_contact_list()
     for home_record in contacts_home:
         db_record = list(filter(lambda x: x.id == home_record.id, contacts))[0]
-        assert db_record.fname.strip() == home_record.fname.strip()
-        assert db_record.lname.strip() == home_record.lname.strip()
+        assert str(db_record.fname).strip() == str(home_record.fname).strip()
+        assert str(db_record.lname).strip() == str(home_record.lname).strip()
         home_record.all_emails = "None\nNone\nNone" if home_record.all_emails == None else home_record.all_emails
         assert db_record.all_emails == home_record.all_emails
         home_record.all_phones = "None\nNone\nNone\nNone" if home_record.all_phones == None else home_record.all_phones
